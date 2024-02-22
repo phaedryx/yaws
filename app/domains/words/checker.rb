e66ceedaeda91fac16ea332d32b0a28f1611e20e
Tad Thorley <phaedryx@users.noreply.github.com>
2024-02-22 11:04:39 -0700

    moar tests

--- app/domains/words/checker.rb
+++ app/domains/words/checker.rb
@@ -2,5 +2,41 @@
 
 module Words
   class Checker < ::ServiceObject
+    step :alpha_validation
+    step :lowercase_validation
+    step :length_validation
+    step :exact_match_validation
+
+    private
+
+    def alpha_validation(guess:, actual:)
+      return Failure(:non_alpha_error) if /[^A-Za-z]/.match?(guess)
+
+      Success(guess:, actual:)
+    end
+
+    def lowercase_validation(guess:, actual:)
+      return Failure(:upper_case_error) if /[A-Z]/.match?(guess)
+
+      Success(guess:, actual:)
+    end
+
+    def length_validation(guess:, actual:)
+      return Failure(:length_error) if guess.length != actual.length
+
+      Success(guess:, actual:)
+    end
+
+    def exact_match_validation(guess:, actual:)
+      return Success([1]) if guess == actual
+
+      Failure(guess:, actual:)
+    end
+
+    # def validate_guess(guess:, actual:)
+    #   return Failure(:non_alpha_error) if /[^A-Za-z]/.match?(guess)
+
+    #   Success(:placeholder)
+    # end
   end
 end
--- test/domains/words/checker_test.rb
+++ test/domains/words/checker_test.rb
@@ -16,10 +16,45 @@ describe ::Words::Checker do
   end
 
   describe ".call" do
-    it "fails if the guess has non-alpha characters"
-    it "fails if the guess has uppercase characters"
-    it "fails if the guess is less than 5 characters"
-    it "fails if the guess is more than 5 characters"
-    it "returns [1,-1,-1,-1,-1] if there is an exact match on the first letter only"
+    it "fails if the guess has non-alpha characters" do
+      @guess = "B2_4A"
+      @actual = "aaaaa"
+
+      _(result.failure?).must_equal(true)
+      _(result.failure).must_equal(:non_alpha_error)
+    end
+
+    it "fails if the guess has uppercase characters" do
+      @guess =  "Aaaaa"
+      @actual = "aaaaa"
+
+      _(result.failure?).must_equal(true)
+      _(result.failure).must_equal(:upper_case_error)
+    end
+
+    it "fails if the guess length is different than actual length" do
+      @guess =  "aaaa"
+      @actual = "aaaaa"
+
+      _(result.failure?).must_equal(true)
+      _(result.failure).must_equal(:length_error)
+    end
+
+    it "returns [1] for guess `a` and actual `a` " do
+      @guess = "a"
+      @actual = "a"
+
+      _(result.success?).must_equal(true)
+      _(result.success).must_equal([1])
+    end
+
+    it "returns an array of 1's equal to the length of the guess if it is an exact match" do
+      length = rand(20)
+      @actual = ("a".."z").to_a.sample(length).join
+      @guess = @actual
+
+      _(result.success?).must_equal(true)
+      _(result.success).must_equal(Array.new(length, 1))
+    end
   end
 end
